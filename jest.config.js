59045902a1a97d7802fc0d472277adc32b207080
Max Potasio <maxpotasio@Maxs-MacBook-Pro.local>
2022-10-12 20:27:50 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@
 module.exports = {
   roots: ["<rootDir>/src"],
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!**/test/**", "**/config/**"],
   testEnvironment: "node",
   transform: {
     ".+\\.ts": "ts-jest",
