45e45f0ac2232a9c7b20f61c465b7f298c31da45
Max Potasio <maxpotasio@Maxs-MacBook-Pro.local>
2022-10-26 20:37:51 -0300

    feat: create use case test

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,17 +1,54 @@
+import { InvalidEmailError } from '../../entities/errors/InvalidEmailError'
+import { InvalidNameError } from '../../entities/errors/invalidNameError'
 import { UserData } from '../../entities/user-data'
+import { left } from '../../shared/either'
+import { UserRepository } from '../ports/user-repository'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
-    console.log(users)
-    // const repo: UserRepository = new InMemoryUserRepository(users)
-    // const usecase: RegisterUserOnMailingList = RegisterUserOnMailingList(repo)
-    // const name = 'any_name'
-    // const email = 'any@email.com'
-    // const response = await usecase.registerUserOnMailingList({ name, email })
 
-    // const user = repo.findUserByEmail(email)
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const response = await usecase.perform({ name, email })
 
-    // expect((await user).name).toBe(name)
+    const user = await repo.findUserByEmail(email)
+
+    expect(user.name).toBe(name)
+    expect(response.value.name).toBe('any_name')
+  })
+
+  test('should not add user with invalid email', async () => {
+    const users: UserData[] = []
+
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const invalidEmail = 'anyemail.com'
+    const response = await usecase.perform({ name, email: invalidEmail })
+
+    const user = await repo.findUserByEmail(invalidEmail)
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not add user with invalid name', async () => {
+    const users: UserData[] = []
+
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const invalidName = ' '
+    const email = 'any@email.com'
+    const response = await usecase.perform({ name: invalidName, email })
+
+    const user = await repo.findUserByEmail(email)
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidNameError()))
   })
 })
